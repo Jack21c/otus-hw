31c77ed4b16fbd508da9e10a389b9014bc393600
Jack21c <80285429+Jack21c@users.noreply.github.com>
2025-02-19 16:50:49 +0300

    HW 2

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,38 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	var res string
+	var buffer string
+	for _, r := range s {
+		// Проверяем, является ли следующий символ цифрой
+		if unicode.IsDigit(r) {
+			// Числа запрещены
+			if buffer == "" {
+				return "", ErrInvalidString
+			}
+			countString := fmt.Sprintf("%c", r)
+			count, _ := strconv.Atoi(countString)
+			res += strings.Repeat(buffer, count)
+			// Очищаем буфер, тк он уже был добавлен к ответу
+			buffer = ""
+			continue
+		}
+		// Добавляем значение буфера к ответу
+		res += buffer
+		// Обновляем буфер текущим символом
+		buffer = fmt.Sprintf("%c", r)
+	}
+	// Добавляем последний символ к ответу
+	res += buffer
+
+	return res, nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -18,6 +18,9 @@ func TestUnpack(t *testing.T) {
 		{input: "aaa0b", expected: "aab"},
 		{input: "🙃0", expected: ""},
 		{input: "aaф0b", expected: "aab"},
+		{input: "aa b\t", expected: "aa b\t"},
+		{input: "aa\n3b", expected: "aa\n\n\nb"},
+		{input: "a1\t2be3:)7", expected: "a\t\tbeee:)))))))"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
@@ -36,7 +39,7 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", "Tes 33p"}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
