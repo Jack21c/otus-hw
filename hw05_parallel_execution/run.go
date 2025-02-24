866f04de8e502a16c217a76f119c1e3efd1f7a65
Jack21c <80285429+Jack21c@users.noreply.github.com>
2025-02-24 18:11:18 +0300

    fix HW5

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -15,48 +15,43 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	errChanMain := make(chan error)
-	errsFromRoutines := make(map[int]chan error, n)
+	errChan := make(chan error)
 	var allowed atomic.Bool
 	allowed.Store(true)
 	taskChan := toChan(tasks)
 
 	wg := new(sync.WaitGroup)
 	for i := 0; i < n; i++ {
-		errsFromRoutines[i] = make(chan error)
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			routine(taskChan, errsFromRoutines[i], &allowed)
+			routine(taskChan, errChan, &allowed)
 		}()
 	}
 
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		accumulateChans(errsFromRoutines, errChanMain, wg)
-	}()
-
 	var err error
-	counter := 0
-	for err = range errChanMain {
+	countErrors := 0
+	countFinished := 0
+	for countFinished < n {
+		err = <-errChan
 		if err != nil {
+			if errors.Is(err, ErrTasksFinished) {
+				allowed.Store(false)
+				countFinished++
+				continue
+			}
 			if !allowed.Load() {
 				continue
 			}
-			if errors.Is(err, ErrTasksFinished) {
-				allowed.Store(false)
-				continue
-			}
-			counter++
-			if m > 0 && counter >= m { // If m <= 0 then errors ignored
+			countErrors++
+			if m > 0 && countErrors >= m { // If m <= 0 then errors ignored
 				allowed.Store(false)
 			}
 		}
 	}
 
 	wg.Wait()
-	if m > 0 && counter >= m {
+	if m > 0 && countErrors >= m {
 		return ErrErrorsLimitExceeded
 	}
 	return nil
@@ -83,32 +78,8 @@ func routine(
 		if ok {
 			errChan <- f()
 		} else {
-			errChan <- ErrTasksFinished
-			close(errChan)
-			return
+			break
 		}
 	}
-	close(errChan)
-}
-
-func accumulateChans(
-	errsFromRoutines map[int]chan error,
-	errChan chan error,
-	wg *sync.WaitGroup,
-) {
-	stop := make(chan struct{})
-	for _, ch := range errsFromRoutines {
-		wg.Add(1)
-		go func(to, from chan error) {
-			defer wg.Done()
-			for err := range from {
-				to <- err
-			}
-			stop <- struct{}{}
-		}(errChan, ch)
-	}
-	for i := 0; i < len(errsFromRoutines); i++ {
-		<-stop
-	}
-	close(errChan)
+	errChan <- ErrTasksFinished
 }
