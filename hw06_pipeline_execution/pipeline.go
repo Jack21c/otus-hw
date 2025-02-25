f38c24b61cc4fc1a012b37d3abd4d09c7efe8328
Jack21c <80285429+Jack21c@users.noreply.github.com>
2025-02-25 18:57:06 +0300

    HW6 without TestAllStageStop

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,30 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	mapOut := make(map[int]Bi, len(stages))
+	for i := 0; i < len(stages); i++ {
+		mapOut[i] = make(Bi)
+	}
+
+	for i, stage := range stages {
+		var inLocal In
+		if i == 0 {
+			inLocal = in
+		} else {
+			inLocal = mapOut[i-1]
+		}
+		go func(in In, stage Stage, out Bi) {
+			defer close(out)
+			for v := range stage(in) {
+				if done != nil {
+					if _, ok := <-done; !ok {
+						break
+					}
+				}
+				out <- v
+			}
+		}(inLocal, stage, mapOut[i])
+	}
+
+	return mapOut[len(stages)-1]
 }
